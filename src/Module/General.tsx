54890c7d3c61437d0de0a6935ae7dda60c8ed362
Harsha Vardhan <hv2003789@gmail.com>
2023-06-26 10:16:42 +0530

    discord link updated

--- src/Module/General.tsx
+++ src/Module/General.tsx
@@ -31,13 +31,13 @@ const TOP_SECTION = {
   Typed_effect: ['72 hours of creation', 'Win awesome prizes'],
   SHORT_DESCRIPTION:
     'Join us on 15th July 2023 with hundreds of students from across the nation for 72 hours of creation, innovation, & fun.',
-  DISCORD_LINK: 'https://discord.gg/Ct9CN3ae',
+  DISCORD_LINK: 'https://discord.gg/A5uBMv7Zs8',
   HACKERS_REGISTRATION_FORM_LINK: 'https://code-relay.devfolio.co/'
 };
 
 const SOCIALS = {
   instagram: 'https://www.instagram.com/webnd.iitbbs',
-  discord: 'https://discord.gg/Ct9CN3ae',
+  discord: 'https://discord.gg/A5uBMv7Zs8',
   linkedin: 'https://www.linkedin.com/company/webd-iitbbs',
   github: 'https://github.com/webd-iitbbs',
   email: 'mailto:secyweb.sg@iitbbs.ac.in',
